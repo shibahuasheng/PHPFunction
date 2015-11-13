d00ee10394878f4312db8d1d258526adb3b6741e
DmoDai <1846907072@qq.com>
2015-11-13 17:01:33 +0800

    #phpexcel ：实现write限制列数

--- phpexcel/excelOperate.php
+++ phpexcel/excelOperate.php
@@ -59,29 +59,14 @@ if(@move_uploaded_file($_FILES[$fileElementName]['tmp_name'],$upFilePath) === FA
 //开始处理excel
 //excel验证（标识符）todo
 //读取excel
-//require_once('Classes/PHPExcel/Reader/Excel2007.php');
-//$objReader = new PHPExcel_Reader_Excel2007;
-//$objPHPExcel = $objReader->load("$_FILES[$fileElementName]['tmp_name']");
 //将include设置为./Classses/ 路径
+
 set_include_path(get_include_path() . PATH_SEPARATOR . './Classes/');
 include 'PHPExcel/IOFactory.php';
 
 $exceldata = excelRead(($_FILES[$fileElementName]['tmp_name']));
 excelWrite('data',$a = array(), $exceldata, false, 5);
-//或者$objWriter = new PHPExcel_Writer_Excel5($objPHPExcel); 非2007格式
-//$objWriter->save("a.xlsx");  存储excel
 
-/*$objWriter = new PHPExcel_Writer_Excel5($objPHPExcel);
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control:must-revalidate, post-check=0, pre-check=0;");
-header("Content-Type:application/force-download");
-header("Content-Type:application/vnd.ms-execl");
-header("Content-Type:application/octet-stream");
-header("Content-Type:application/download");;
-header('Content-Disposition:attachment;filename="resume.xls"');
-header("Content-Transfer-Encoding:binary");
-$objWriter->save('php://output');*/
 
 
 function excelRead($filename){
@@ -110,6 +95,7 @@ function excelRead($filename){
  * @param array $data  一个二维数组,结构如同从数据库查出来的数组
  * @param array $title excel的第一行标题,一个数组,如果为空则没有标题
  * @param string $filename  下载的文件名
+ * 但是对07以上的excel出错
  *
  * exportexcel($arr,array('id','账户','密码','昵称'),'文件名!');
  */
@@ -159,125 +145,125 @@ function getExcelColumnValue($index){
  * @param bool|false $local  默认浏览器下载excel
  * @throws PHPExcel_Exception
  */
-    function excelWrite($fileName, $headArr, $data,$local = false, $colNum = false)
-    {
-        if (empty($data) || !is_array($data)) {
-            die("data must be a array");
-        }
-        if (empty($fileName)) {
-            exit;
-        }
-        if(is_numeric($colNum) && is_int($colNum+0) && ((int)$colNum > 0) ) {
-            $colNumCount = true;
-        }
-        $date = date("Y_m_d", time());
-       /* if($local) {
-       //03兼容格式
-            $fileName .= "_{$date}.xls";
-        }else{
-            $fileName .= "_{$date}.xlsx";
-        }*/
-        $fileName .= "_{$date}.xls";
+function excelWrite($fileName, $headArr, $data,$local = false, $colNum = false)
+{
+    if (empty($data) || !is_array($data)) {
+        die("data must be a array");
+    }
+    if (empty($fileName)) {
+        exit;
+    }
+    if(is_numeric($colNum) && is_int($colNum+0) && ((int)$colNum > 0) ) {
+        $colNumCount = true;
+    }
+    $date = date("Y_m_d", time());
+    /* if($local) {
+    //03兼容格式
+         $fileName .= "_{$date}.xls";
+     }else{
+         $fileName .= "_{$date}.xlsx";
+     }*/
+    $fileName .= "_{$date}.xls";
 
-        //创建新的PHPExcel对象
-        $objPHPExcel = new PHPExcel();
-        $objProps = $objPHPExcel->getProperties();
+    //创建新的PHPExcel对象
+    $objPHPExcel = new PHPExcel();
+    $objProps = $objPHPExcel->getProperties();
 
-        if(is_array($headArr) && !empty($headArr)){
-            //设置表头
-            $columindex = 0;
-                foreach ($headArr as $key => $v) {
-                    if($colNumCount ) {
-                        if(((int)$columindex < (int)$colNum)) {
-                            $colum = getExcelColumnValue($columindex);
-                            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($colum . '1', $v);
-                            $columindex++;
-                        }
-                    }else{
-                        $colum = getExcelColumnValue($columindex);
-                        $objPHPExcel->setActiveSheetIndex(0)->setCellValue($colum . '1', $v);
-                        $columindex++;
-                    }
+    if(is_array($headArr) && !empty($headArr)){
+        //设置表头
+        $columindex = 0;
+        foreach ($headArr as $key => $v) {
+            if($colNumCount ) {
+                if(((int)$columindex < (int)$colNum)) {
+                    $colum = getExcelColumnValue($columindex);
+                    $objPHPExcel->setActiveSheetIndex(0)->setCellValue($colum . '1', $v);
+                    $columindex++;
                 }
-
-            $row = 2;
-        }else{
-            $row = 1;
+            }else{
+                $colum = getExcelColumnValue($columindex);
+                $objPHPExcel->setActiveSheetIndex(0)->setCellValue($colum . '1', $v);
+                $columindex++;
+            }
         }
 
-        $objActSheet = $objPHPExcel->getActiveSheet();
-        $objPHPExcel->getActiveSheet()->getStyle( 'A1:E1')->getFill()->getStartColor()->setARGB('FF0094FF');
-        //$objPHPExcel->getActiveSheet()->getStyle( 'A1:E1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
-        /*$objPHPExcel->getActiveSheet()->getStyle( 'A3:A10')->applyFromArray(
-            array(
-                'font'    => array (
-                    'bold'      => true
+        $row = 2;
+    }else{
+        $row = 1;
+    }
+
+    $objActSheet = $objPHPExcel->getActiveSheet();
+    $objPHPExcel->getActiveSheet()->getStyle( 'A1:E1')->getFill()->getStartColor()->setARGB('FF0094FF');
+    //$objPHPExcel->getActiveSheet()->getStyle( 'A1:E1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
+    /*$objPHPExcel->getActiveSheet()->getStyle( 'A3:A10')->applyFromArray(
+        array(
+            'font'    => array (
+                'bold'      => true
+            ),
+            'alignment' => array (
+                'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_RIGHT ,
+            ),
+            'borders' => array (
+                'top'     => array (
+                    'style' => PHPExcel_Style_Border::BORDER_THIN
+                )
+            ),
+            'fill' => array (
+                'type'       => PHPExcel_Style_Fill::FILL_GRADIENT_LINEAR ,
+                'rotation'   => 90,
+                'startcolor' => array (
+                    'argb' => 'FFA0A0A0'
                 ),
-                'alignment' => array (
-                    'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_RIGHT ,
-                ),
-                'borders' => array (
-                    'top'     => array (
-                        'style' => PHPExcel_Style_Border::BORDER_THIN
-                    )
-                ),
-                'fill' => array (
-                    'type'       => PHPExcel_Style_Fill::FILL_GRADIENT_LINEAR ,
-                    'rotation'   => 90,
-                    'startcolor' => array (
-                        'argb' => 'FFA0A0A0'
-                    ),
-                    'endcolor'   => array (
-                        'argb' => 'FF0094FF'
-                    )
+                'endcolor'   => array (
+                    'argb' => 'FF0094FF'
                 )
             )
-        );*/
+        )
+    );*/
 
-        foreach ($data as $key => $rows) { //行写入
-            $columindex = 0;
-            foreach ($rows as $keyName => $value) {// 列写入
-                if($colNumCount){
-                    if(((int)$columindex < (int)$colNum)) {
-                        $colum = getExcelColumnValue($columindex);
-                        //宽度
-                        //$objActSheet->getColumnDimension($colum)->setAutoSize(true);
-                        //颜色
-                        $objPHPExcel->getActiveSheet()->getStyle('A1')->getFill()->getStartColor()->setARGB('FF808080');
-                        $objActSheet->setCellValue($colum . $row, $value);
-                        $columindex++;
-                    }
-                } else {
+    foreach ($data as $key => $rows) { //行写入
+        $columindex = 0;
+        foreach ($rows as $keyName => $value) {// 列写入
+            if($colNumCount){
+                if(((int)$columindex < (int)$colNum)) {
                     $colum = getExcelColumnValue($columindex);
                     //宽度
                     //$objActSheet->getColumnDimension($colum)->setAutoSize(true);
                     //颜色
-                //$objPHPExcel->getActiveSheet()->getStyle('A1')->getFill()->getStartColor()->setARGB('FF808080');
+                    $objPHPExcel->getActiveSheet()->getStyle('A1')->getFill()->getStartColor()->setARGB('FF808080');
                     $objActSheet->setCellValue($colum . $row, $value);
                     $columindex++;
                 }
+            } else {
+                $colum = getExcelColumnValue($columindex);
+                //宽度
+                //$objActSheet->getColumnDimension($colum)->setAutoSize(true);
+                //颜色
+                //$objPHPExcel->getActiveSheet()->getStyle('A1')->getFill()->getStartColor()->setARGB('FF808080');
+                $objActSheet->setCellValue($colum . $row, $value);
+                $columindex++;
             }
-            $row++;
         }
+        $row++;
+    }
 
-        $fileName = iconv("utf-8", "gb2312", $fileName);
-        //重命名表
-        $objPHPExcel->getActiveSheet()->setTitle('Simple');
-        //设置活动单指数到第一个表,所以Excel打开这是第一个表
-        $objPHPExcel->setActiveSheetIndex(0);
-        //将输出重定向到一个客户端web浏览器(Excel2007)
+    $fileName = iconv("utf-8", "gb2312", $fileName);
+    //重命名表
+    $objPHPExcel->getActiveSheet()->setTitle('Simple');
+    //设置活动单指数到第一个表,所以Excel打开这是第一个表
+    $objPHPExcel->setActiveSheetIndex(0);
+    //将输出重定向到一个客户端web浏览器(Excel2007)
 
-        $objWriter = new PHPExcel_Writer_Excel5($objPHPExcel);
+    $objWriter = new PHPExcel_Writer_Excel5($objPHPExcel);
 
-        if($local) {
-            $objWriter->save($fileName); //脚本方式运行，保存在当前目录
-        }else{
-            //$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');这个中可以输出xlsx
-            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-            header("Content-Disposition: attachment; filename=\"$fileName\"");
-            header('Cache-Control: max-age=0');
-            $objWriter->save('php://output');
-        }
-        exit;
+    if($local) {
+        $objWriter->save($fileName); //脚本方式运行，保存在当前目录
+    }else{
+        //$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');这个中可以输出xlsx
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header("Content-Disposition: attachment; filename=\"$fileName\"");
+        header('Cache-Control: max-age=0');
+        $objWriter->save('php://output');
+    }
+    exit;
 
 }
